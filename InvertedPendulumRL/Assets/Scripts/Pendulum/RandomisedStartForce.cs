899c9dac40677bec529cdd46733274c5e4d29ac8
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-12-27 14:18:44 +1300

    Randomised Pendulum Force On Start

---
+++ InvertedPendulumRL/Assets/Scripts/Pendulum/RandomisedStartForce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomisedStartForce : MonoBehaviour {
+	
+	private Vector2 force;
+	private Rigidbody2D rb2d;
+	
+	// Start is called before the first frame update
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+		force.x = Random.Range(-5,5);
+		force.y = 0;
+		rb2d.AddForce(force, ForceMode2D.Impulse);
+	}
+}
+
