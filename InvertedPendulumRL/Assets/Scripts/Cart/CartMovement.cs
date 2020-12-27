099cee6dbb93ce58ede6ed65673552ac1ea24b47
Joshua-Benfell <joshuabenfell@hotmail.co.nz>
2020-12-27 14:18:28 +1300

    Add User Controlled Movement for Testing

---
+++ InvertedPendulumRL/Assets/Scripts/Cart/CartMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartMovement : MonoBehaviour {
+	// Start is called before the first frame update
+
+	public float speed = 100f;
+	private Rigidbody2D rb2d;
+	private float moveHorizontal;
+
+
+	void Start() {
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// FixedUpdate is called at a fixed time interval
+	void FixedUpdate() {
+		moveHorizontal = Input.GetAxis("Horizontal");
+		Vector2 movement = new Vector2(moveHorizontal, 0);
+		rb2d.AddForce(movement*speed);
+	}
+}
